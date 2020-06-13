2b64251e6038c333d183dae612c3c8953ba2f279
Mubarak <12859241+FireDeclan@users.noreply.github.com>
2020-06-13 18:03:05 +0100

    Added default user

--- src/zenxu/Startup.cs
+++ src/zenxu/Startup.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Zenxu.Data.DatabaseContexts.ApplicationDbContext;
 using Zenxu.Data.DatabaseContexts.AuthenticationDbContext;
+using Zenxu.Data.Entities;
 
 namespace zenxu
 {
@@ -37,11 +39,26 @@ namespace zenxu
                     sqlServerOptions.MigrationsAssembly("Zenxu.Data");
                 })
             );
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AuthenticationDbContext>()
+            .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options => 
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
             if (env.IsDevelopment())
             {
@@ -66,6 +83,52 @@ namespace zenxu
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            MigrateDatabaseContexts(svp);
+            CreateDefaultUserAndRole(svp).GetAwaiter().GetResult();
+        }
+
+        public void MigrateDatabaseContexts(IServiceProvider svp)
+        {
+            var authenticationDbContext = svp.GetRequiredService<AuthenticationDbContext>();
+            authenticationDbContext.Database.Migrate();
+
+            var applicationDbContext = svp.GetRequiredService<ApplicationDbContext>();
+            applicationDbContext.Database.Migrate();
+        }
+
+        public async Task CreateDefaultUserAndRole(IServiceProvider svp)
+        {
+            string[] roles = new string[] {"SystemAdministrator", "Agent", "User"};
+            var userEmail = "admin@zenxu.com";
+            var userPassword = "SuperSecretPassword@2020";
+
+            var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>>();
+            foreach(var role in roles)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if(!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole{Name = role});
+                }
+            }
+
+            var userManager = svp.GetRequiredService<UserManager<ApplicationUser>>();
+            var user = await userManager.FindByEmailAsync(userEmail);
+            if(user is null)
+            {
+                user = new ApplicationUser
+                {
+                    Email = userEmail,
+                    UserName = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "+23412345678",
+                    PhoneNumberConfirmed = true
+                };
+
+                await userManager.CreateAsync(user, userPassword);
+                await userManager.AddToRolesAsync(user, roles);
+            }
         }
     }
 }
